ac1ed47e2bba82fc6a2ab2c448b4cddcbdef74b6
Jiyun Lee <02leejiyun@naver.com>
2023-01-10 14:35:52 +0900

    fix: [ft_strchr] libft.h 헤더 추가

--- 00_libft/ft_strchr.c
+++ 00_libft/ft_strchr.c
@@ -6,9 +6,11 @@
 /*   By: jiyunlee <jiyunlee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 20:35:17 by jiyunlee          #+#    #+#             */
-/*   Updated: 2022/11/21 15:12:38 by jiyunlee         ###   ########.fr       */
+/*   Updated: 2023/01/10 14:35:40 by jiyunlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 char	*ft_strchr(const char *s, int c)
 {
