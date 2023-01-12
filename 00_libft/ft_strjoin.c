13109cc93b2eff475ae1526dfc630579bf63a30f
jiyuneel <02leejiyun@naver.com>
2023-01-13 00:36:46 +0900

    refactor: [ft_strjoin] 반환값 0을 NULL로 수정

--- 00_libft/ft_strjoin.c
+++ 00_libft/ft_strjoin.c
@@ -20,7 +20,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	len = ft_strlen(s1) + ft_strlen(s2);
 	str = (char *)malloc(sizeof(char) * len + 1);
 	if (!str)
-		return (0);
+		return (NULL);
 	ft_strlcpy(str, s1, ft_strlen(s1) + 1);
 	ft_strlcpy(str + ft_strlen(s1), s2, ft_strlen(s2) + 1);
 	return (str);
