cfaefe497a56a250948fe8f517366c9e2859c20e
Jiyun Lee <02leejiyun@naver.com>
2023-01-20 21:08:07 +0900

    refactor: [ft_substr] 반환값 0을 NULL로 수정

--- 00_libft/ft_substr.c
+++ 00_libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: jiyunlee <jiyunlee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 18:06:33 by jiyunlee          #+#    #+#             */
-/*   Updated: 2022/11/17 17:17:03 by jiyunlee         ###   ########.fr       */
+/*   Updated: 2023/01/20 21:07:44 by jiyunlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		len = ft_strlen(s) - start;
 	str = (char *)malloc(sizeof(char) * len + 1);
 	if (!str)
-		return (0);
+		return (NULL);
 	i = 0;
 	while ((s + start)[i] && i < len)
 	{
