103c55e356f679f2924082892bc60b689199ee1d
이지윤 <02leejiyun@naver.com>
2023-02-02 06:19:23 -0800

    add: [circle 02] push_swap.h

---
+++ 02_push_swap/push_swap.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiyunlee <jiyunlee@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/02 06:05:38 by jiyunlee          #+#    #+#             */
+/*   Updated: 2023/02/02 06:16:35 by jiyunlee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct s_list
+{
+	int				val;
+	int             idx;
+	struct s_list* prev;
+	struct s_list* next;
+}	t_list;
+
+typedef struct s_stack
+{
+	int				len;
+	struct s_list*	top;
+}	t_stack;
+
+#endif
