f33ef6e25e6955d7fc2ca1a70a15374b32966671
Jiyun Lee <02leejiyun@naver.com>
2023-02-03 16:35:43 +0900

    feat: [push_swap.h] stack 구조체에 name 요소 추가

--- 02_push_swap/push_swap.h
+++ 02_push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jiyunlee <jiyunlee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 06:05:38 by jiyunlee          #+#    #+#             */
-/*   Updated: 2023/02/03 14:33:23 by jiyunlee         ###   ########.fr       */
+/*   Updated: 2023/02/03 16:34:24 by jiyunlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,12 @@ typedef struct s_list
 
 typedef struct s_stack
 {
+	char			name;
 	int				len;
 	struct s_list	*top;
 }	t_stack;
 
+void	ft_lstadd_back(t_stack *stack, t_list *new);
 void	stack_init(t_stack *a, t_stack *b, char **argv);
 
 #endif
