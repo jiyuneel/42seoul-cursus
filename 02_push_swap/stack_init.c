9722e66088143700ddf15be90f066e4556dadc52
Jiyun Lee <02leejiyun@naver.com>
2023-02-03 16:23:14 +0900

    feat: [stack_init] val 크기 순으로 idx 설정

--- 02_push_swap/stack_init.c
+++ 02_push_swap/stack_init.c
@@ -6,7 +6,7 @@
 /*   By: jiyunlee <jiyunlee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 14:25:31 by jiyunlee          #+#    #+#             */
-/*   Updated: 2023/02/03 14:28:33 by jiyunlee         ###   ########.fr       */
+/*   Updated: 2023/02/03 14:42:24 by jiyunlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,32 @@ void	ft_lstadd_back(t_stack *stack, t_list *new)
 	stack->len++;
 }
 
+void	set_idx(t_stack *stack)
+{
+	t_list	*tmp1;
+	t_list	*tmp2;
+	int		cnt;
+
+	tmp1 = stack->top;
+	while (1)
+	{
+		cnt = 0;
+		tmp2 = stack->top;
+		while (1)
+		{
+			if (tmp1->val > tmp2->val)
+				cnt++;
+			tmp2 = tmp2->next;
+			if (tmp2 == stack->top)
+				break ;
+		}
+		tmp1->idx = cnt;
+		tmp1 = tmp1->next;
+		if (tmp1 == stack->top)
+			break ;
+	}
+}
+
 void	stack_init(t_stack *a, t_stack *b, char **argv)
 {
 	int	i;
@@ -54,4 +80,5 @@ void	stack_init(t_stack *a, t_stack *b, char **argv)
 	i = 1;
 	while (argv[i])
 		ft_lstadd_back(a, ft_lstnew(atoi(argv[i++])));
+	set_idx(a);
 }
