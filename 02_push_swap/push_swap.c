2bae51355ec2826764d4e98e12ba3e3bb019fa1f
이지윤 <02leejiyun@naver.com>
2023-02-05 04:20:18 +0900

    feat: [push_swap] b_to_a 함수 구현

--- 02_push_swap/push_swap.c
+++ 02_push_swap/push_swap.c
@@ -6,14 +6,14 @@
 /*   By: jiyunlee <jiyunlee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 06:03:48 by jiyunlee          #+#    #+#             */
-/*   Updated: 2023/02/05 03:58:39 by jiyunlee         ###   ########.fr       */
+/*   Updated: 2023/02/05 04:19:40 by jiyunlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 void	a_to_b(t_stack *a, t_stack *b);
-// void	b_to_a(t_stack *a, t_stack *b);
+void	b_to_a(t_stack *a, t_stack *b);
 
 int	main(int argc, char *argv[])
 {
@@ -22,9 +22,9 @@ int	main(int argc, char *argv[])
 
 	if (argc == 1)
 		return (0);
-	stack_init(&a, &b, argv + 1);
+	stack_init(&a, &b, ++argv);
 	a_to_b(&a, &b);
-	// b_to_a(&a, &b);
+	b_to_a(&a, &b);
 }
 
 void	a_to_b(t_stack *a, t_stack *b)
@@ -51,5 +51,51 @@ void	a_to_b(t_stack *a, t_stack *b)
 		}
 		else if (top > i + scale)
 			rotate(a);
+	}
+}
+
+int	get_big_idx(t_stack *stack)
+{
+	t_list	*tmp;
+	int		i;
+
+	tmp = stack->top;
+	i = 0;
+	while (1)
+	{
+		if (tmp->idx == stack->len - 1)
+			break ;
+		tmp = tmp->next;
+		i++;
+	}
+	return (i);
+}
+
+void	b_to_a(t_stack *a, t_stack *b)
+{
+	int	big_idx;
+	int	mid;
+
+	while (b->len)
+	{
+		big_idx = get_big_idx(b);
+		mid = b->len / 2;
+		if (big_idx > mid)
+		{
+			while (big_idx < b->len)
+			{
+				rev_rotate(b);
+				big_idx++;
+			}
+		}
+		else if (big_idx <= mid)
+		{
+			while (big_idx > 0)
+			{
+				rotate(b);
+				big_idx--;
+			}
+		}
+		push(b, a);
 	}
 }
