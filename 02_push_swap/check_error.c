e21b00731aa0d8377418ce119e4db363243bc5ad
이지윤 <02leejiyun@naver.com>
2023-02-04 18:46:53 +0900

    feat: [check_error] 인자값이 정수 범위 밖일 때 에러 처리

--- 02_push_swap/check_error.c
+++ 02_push_swap/check_error.c
@@ -6,7 +6,7 @@
 /*   By: jiyunlee <jiyunlee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 18:30:35 by jiyunlee          #+#    #+#             */
-/*   Updated: 2023/02/04 18:45:00 by jiyunlee         ###   ########.fr       */
+/*   Updated: 2023/02/04 18:46:33 by jiyunlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,28 @@ int	ft_isdigit(int c)
 	if ('0' <= c && c <= '9')
 		return (c);
 	return (0);
+}
+
+long long	ft_atoi(const char *str)
+{
+	long long	num;
+	int			sign;
+
+	num = 0;
+	sign = 1;
+	if (*str == '+')
+		str++;
+	else if (*str == '-')
+	{
+		sign = -1;
+		str++;
+	}
+	while (*str && ft_isdigit(*str))
+	{
+		num = num * 10 + (*str - '0');
+		str++;
+	}
+	return (sign * num);
 }
 
 int	check_digit(char *str)
@@ -34,9 +56,17 @@ int	check_digit(char *str)
 	return (1);
 }
 
+int	check_range(long long num)
+{
+	if (num < -2147483648 || num > 2147483647)
+		return (0);
+	return (1);
+}
+
 void	check_error(t_stack *stack, char *arg_str, long long *arg_int)
 {
-	if (!check_digit(arg_str))
+	*arg_int = ft_atoi(arg_str);
+	if (!check_digit(arg_str) || !check_range(*arg_int))
 	{
 		// free_list();
 		write(2, "Error\n", 6);
