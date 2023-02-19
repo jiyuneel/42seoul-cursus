5c0a881673c7f2e6ca5d6de09d186c4d5371e6d5
이지윤 <02leejiyun@naver.com>
2023-02-19 23:20:40 +0900

    refactor: [ft_strlcpy] 문자열 마지막의 0을 '\0'로 수정

--- 00_libft/ft_strlcpy.c
+++ 00_libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: jiyunlee <jiyunlee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 16:32:05 by jiyunlee          #+#    #+#             */
-/*   Updated: 2022/11/16 21:31:35 by jiyunlee         ###   ########.fr       */
+/*   Updated: 2023/02/19 23:18:05 by jiyunlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,6 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t dstsize)
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = 0;
+	dest[i] = '\0';
 	return (ft_strlen(src));
 }
