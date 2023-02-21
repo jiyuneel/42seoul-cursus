8a3e973624c2a5bc0cff73779916f7e0ef8e5ec0
이지윤 <02leejiyun@naver.com>
2023-02-21 20:43:12 +0900

    feat: [server] 수신한 문자가 127인 경우 'end' 출력

--- 02_minitalk/server.c
+++ 02_minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: jiyunlee <jiyunlee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 23:24:12 by jiyunlee          #+#    #+#             */
-/*   Updated: 2023/02/21 20:03:38 by jiyunlee         ###   ########.fr       */
+/*   Updated: 2023/02/21 20:39:05 by jiyunlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,14 +32,14 @@ void	receive_signal(int sig)
 	if (sig == SIGUSR1)
 		signal.chr <<= 1;
 	else if (sig == SIGUSR2)
-	{
-		signal.chr <<= 1;
-		signal.chr |= 1;
-	}
+		signal.chr = (signal.chr << 1) | 1;
 	signal.bit++;
 	if (signal.bit == 8)
 	{
-		ft_putchar_fd(signal.chr, 1);
+		if (signal.chr == 127)
+			ft_putstr_fd("end!!\n", 1);
+		else
+			ft_putchar_fd(signal.chr, 1);
 		signal.bit = 0;
 		signal.chr = 0;
 	}
