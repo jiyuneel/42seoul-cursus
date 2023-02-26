73ca4eb3c42a91dde65d7d005ce792c4659a8b45
이지윤 <02leejiyun@naver.com>
2023-02-26 23:28:51 +0900

    refactor: [ft_strdup] 문자열 마지막의 0을 '\0'로 수정

--- 00_libft/ft_strdup.c
+++ 00_libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: jiyunlee <jiyunlee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 17:58:47 by jiyunlee          #+#    #+#             */
-/*   Updated: 2022/11/22 13:57:37 by jiyunlee         ###   ########.fr       */
+/*   Updated: 2023/02/26 23:28:24 by jiyunlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 char	*ft_strdup(const char *s1)
 {
-	char	*ptr;
+	char	*str;
 	size_t	i;
 
-	ptr = (char *)malloc(sizeof(char) * ft_strlen(s1) + 1);
-	if (!ptr)
+	str = (char *)malloc(sizeof(char) * ft_strlen(s1) + 1);
+	if (!str)
 		return (NULL);
 	i = 0;
 	while (s1[i])
 	{
-		ptr[i] = s1[i];
+		str[i] = s1[i];
 		i++;
 	}
-	ptr[i] = 0;
-	return (ptr);
+	str[i] = '\0';
+	return (str);
 }
