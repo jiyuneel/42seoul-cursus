a770fb8a5da62998ef73b1a0df9e9d22b9383997
이지윤 <02leejiyun@naver.com>
2023-07-29 03:55:07 +0900

    feat: [so_long.c] 유효한 파일인지 확인

--- 02_so_long/so_long.c
+++ 02_so_long/so_long.c
@@ -6,75 +6,17 @@
 /*   By: jiyunlee <jiyunlee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 20:29:54 by jiyunlee          #+#    #+#             */
-/*   Updated: 2023/07/28 17:13:15 by jiyunlee         ###   ########.fr       */
+/*   Updated: 2023/07/28 22:34:53 by jiyunlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <fcntl.h>
-
-size_t	ft_strlen(const char *s)
-{
-	size_t	len;
-
-	len = 0;
-	while (s[len])
-		len++;
-	return (len);
-}
-
-int	ft_strcmp(const char *s1, const char *s2)
-{
-	size_t	i;
-
-	i = 0;
-	while (s1[i] && s2[i])
-	{
-		if (s1[i] != s2[i])
-			break ;
-		i++;
-	}
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-}
-
-void	ft_putstr_fd(char *s, int fd)
-{
-	write(fd, s, ft_strlen(s));
-}
-
-void	check_valid_file(int argc, char **argv)
-{
-	int		fd;
-	char	*filename;
-	size_t	len;
-
-	if (argc != 2)
-	{
-		ft_putstr_fd("Error\ninvalid argument\n", 1);
-		exit(1);
-	}
-	filename = argv[1];
-	len = ft_strlen(filename);
-	if (len <= 4 || ft_strcmp(filename + len - 4, ".ber") || filename[len - 5] == '/')
-	{
-		ft_putstr_fd("Error\ninvalid filename\n", 1);
-		exit(1);
-	}
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
-		exit(1);
-}
-
-char **check_valid_map()
-{
-	return NULL;
-}
+#include "so_long.h"
 
 int	main(int argc, char **argv)
 {
-	char	**map;
+	static t_map	m;
 
 	check_valid_file(argc, argv);
-	// map = check_valid_map();
+	check_valid_map(argv[1], &m);
+	// printf("c: %d, e: %d, p: %d\n", m.c, m.e, m.p);
 }
--- 02_so_long/so_long.h
+++ 02_so_long/so_long.h
@@ -6,21 +6,31 @@
 /*   By: jiyunlee <jiyunlee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 17:27:20 by jiyunlee          #+#    #+#             */
-/*   Updated: 2023/07/28 19:43:14 by jiyunlee         ###   ########.fr       */
+/*   Updated: 2023/07/29 02:24:04 by jiyunlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
-# endif
-
+# include "libft/libft.h"
 # include <unistd.h>
 # include <stdlib.h>
 # include <fcntl.h>
+/**/
+# include <stdio.h>
 
-int	check_valid_file(int argc, char **argv);
+typedef struct s_map
+{
+	char	**map;
+	int		width;
+	int		height;
+	int		c;
+	int		e;
+	int		p;
+}	t_map;
+
+void	check_valid_file(int argc, char **argv);
+void	check_valid_map(char *filename, t_map *m);
 
 #endif
