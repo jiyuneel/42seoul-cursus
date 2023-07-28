bb176fcb19f08b1bd8b0e20e219d73016e4c4d4e
이지윤 <02leejiyun@naver.com>
2023-07-28 19:46:56 +0900

    add: [so_long.h] 헤더 파일 추가

---
+++ 02_so_long/so_long.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiyunlee <jiyunlee@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/28 17:27:20 by jiyunlee          #+#    #+#             */
+/*   Updated: 2023/07/28 19:43:14 by jiyunlee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+int	check_valid_file(int argc, char **argv);
+
+#endif
