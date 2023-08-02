d0cb0b74fd7e667cfa2c0d91331b74ea1939acd1
이지윤 <02leejiyun@naver.com>
2023-08-03 01:57:21 +0900

    feat: [check_valid_map.c] map의 C, E, P 개수 확인

--- 02_so_long/check_valid_map.c
+++ 02_so_long/check_valid_map.c
@@ -6,100 +6,11 @@
 /*   By: jiyunlee <jiyunlee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 17:31:12 by jiyunlee          #+#    #+#             */
-/*   Updated: 2023/07/31 13:40:50 by jiyunlee         ###   ########.fr       */
+/*   Updated: 2023/07/31 17:29:11 by jiyunlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-
-void	check_valid_file(int argc, char **argv)
-{
-	int		fd;
-	char	*filename;
-	size_t	len;
-
-	if (argc != 2)
-	{
-		ft_putstr_fd("Error\ninvalid argument\n", 1);
-		exit(1);
-	}
-	filename = argv[1];
-	len = ft_strlen(filename);
-	if (len <= 4 || ft_strncmp(filename + len - 4, ".ber", 4) || filename[len - 5] == '/')
-	{
-		ft_putstr_fd("Error\ninvalid filename\n", 1);
-		exit(1);
-	}
-}
-
-void	get_map_info(char *filename, t_map *m)
-{
-	int		fd;
-	char	*line;
-	size_t	len;
-
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
-		exit(1);
-	while (1)
-	{
-		line = get_next_line(fd);
-		if (!line)
-			break ;
-		if (!m->width)
-		{
-			len = ft_strlen(line);
-			if (line[len - 1] == '\n')
-				m->width = len - 1;
-			else
-				m->width = len;
-		}
-		m->height++;
-		free(line);
-	}
-	if (close(fd) < 0)
-		exit(1);
-}
-
-void	free_arr(char **arr)
-{
-	int	i;
-
-	i = 0;
-	while (arr[i])
-		free(arr[i++]);
-	free(arr);
-	exit(1);
-}
-
-void	get_map(char *filename, t_map *m)
-{
-	int		fd;
-	int		i;
-	char	*line;
-	size_t	len;
-
-	m->map = (char **)malloc(sizeof(char *) * (m->height + 1));
-	fd = open(filename, O_RDONLY);
-	if (!m->map || fd < 0)
-		exit(1);
-	i = 0;
-	while (i < m->height)
-	{
-		line = get_next_line(fd);
-		len = ft_strlen(line);
-		if (line[len - 1] == '\n')
-			len--;
-		m->map[i] = (char *)malloc(sizeof(char) * (len + 1));
-		if (!m->map[i])
-			free_arr(m->map);
-		ft_strlcpy(m->map[i++], line, len + 1);
-		free(line);
-	}
-	m->map[i] = NULL;
-	if (close(fd) < 0)
-		exit(1);
-}
 
 void	check_rectangular(t_map *m)
 {
@@ -109,11 +20,7 @@ void	check_rectangular(t_map *m)
 	while (i < m->height)
 	{
 		if (ft_strlen(m->map[i]) != m->width)
-		{
-			ft_putstr_fd("Error\nThe map must be rectangular.\n", 1);
-			exit(1);
-			// free_arr(m->map);
-		}
+			error_exit("The map must be rectangular.\n");
 		i++;
 	}
 }
@@ -132,25 +39,24 @@ void	check_surrounded(t_map *m)
 			while (m->map[i][j])
 			{
 				if (m->map[i][j] != '1')
-				{
-					ft_putstr_fd("Error\nThe map must be surrounded by walls.\n", 1);
-					exit(1);
-					// free_arr(m->map);
-				}
+					error_exit("The map must be surrounded by walls.\n");
 				j++;
 			}
 		}
-		else
-		{
-			if (m->map[i][0] != '1' || m->map[i][m->width - 1] != '1')
-			{
-				ft_putstr_fd("Error\nThe map must be surrounded by walls.\n", 1);
-				exit(1);
-				// free_arr(m->map);
-			}
-		}
+		else if (m->map[i][0] != '1' || m->map[i][m->width - 1] != '1')
+				error_exit("The map must be surrounded by walls.\n");
 		i++;
 	}
+}
+
+void	check_CEP(t_map *m)
+{
+	if (m->e != 1)
+		error_exit("The map must contain 1 exit.\n");
+	else if (!m->c)
+		error_exit("The map must contain at least 1 collectible.\n");
+	else if (m->p != 1)
+		error_exit("The map must contain 1 starting position.\n");
 }
 
 void	check_component(t_map *m)
@@ -169,13 +75,13 @@ void	check_component(t_map *m)
 			else if (m->map[i][j] == 'E')
 				m->e++;
 			else if (m->map[i][j] == 'P')
-				m->p++;
-			else if (m->map[i][j] != '0' && m->map[i][j] != '1')
 			{
-				ft_putstr_fd("Error\nThe map must be composed of only 5 characters(0, 1, C, E, P).\n", 1);
-				exit(1);
-				// free_arr(m->map);
+				m->p++;
+				m->p_x = j;
+				m->p_y = i;
 			}
+			else if (m->map[i][j] != '0' && m->map[i][j] != '1')
+				error_exit("The map must be composed of only 5 characters(0, 1, C, E, P).\n");
 			j++;
 		}
 		i++;
@@ -184,15 +90,10 @@ void	check_component(t_map *m)
 
 void	check_valid_map(char *filename, t_map *m)
 {
-	int	i;
-	int	j;
-
-	get_map_info(filename, m);
-	// printf("height:%d, width: %d\n", m->height, m->width);
-	get_map(filename, m);
-	// for (i = 0; m->map[i]; i++)
-	// 	printf("%s\n", m->map[i]);
+	map_info_init(filename, m);
+	map_init(filename, m);
 	check_rectangular(m);
 	check_component(m);
+	check_CEP(m);
 	check_surrounded(m);
 }
