aca258142e4a80d5a65d382946dea0b5352a81a6
이지윤 <02leejiyun@naver.com>
2023-08-03 13:18:16 +0900

    refactor: [so_long.h] 함수 및 구조체 설정

--- 02_so_long/so_long.h
+++ 02_so_long/so_long.h
@@ -6,7 +6,7 @@
 /*   By: jiyunlee <jiyunlee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 17:27:20 by jiyunlee          #+#    #+#             */
-/*   Updated: 2023/07/29 02:24:04 by jiyunlee         ###   ########.fr       */
+/*   Updated: 2023/08/03 03:08:38 by jiyunlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define SO_LONG_H
 
 # include "libft/libft.h"
+# include "get_next_line/get_next_line.h"
 # include <unistd.h>
 # include <stdlib.h>
 # include <fcntl.h>
@@ -23,14 +24,22 @@
 typedef struct s_map
 {
 	char	**map;
+	int		**visited;
 	int		width;
 	int		height;
 	int		c;
 	int		e;
 	int		p;
+	int		p_x;
+	int		p_y;
 }	t_map;
 
+void	map_info_init(char *filename, t_map *m);
+void	map_init(char *filename, t_map *m);
 void	check_valid_file(int argc, char **argv);
 void	check_valid_map(char *filename, t_map *m);
+void	check_valid_path(t_map *m);
+void	error_exit(char *str);
+void	visited_init(t_map *m);
 
 #endif
