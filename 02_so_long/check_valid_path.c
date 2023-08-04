1aee1a3b463a3bdf856abc90a385f3e4e4f32149
이지윤 <02leejiyun@naver.com>
2023-08-04 20:49:46 +0900

    feat: [check_valid_path.c] 유효한 경로가 있는지 확인

--- 02_so_long/check_valid_path.c
+++ 02_so_long/check_valid_path.c
@@ -12,7 +12,25 @@
 
 #include "so_long.h"
 
+void	valid_path(t_map *m, int x, int y)
+{
+	if (m->map[y][x] == '1' || m->visited[y][x])
+		return ;
+	m->visited[y][x] = TRUE;
+	if (m->map[y][x] == 'E')
+		m->e_flag = TRUE;
+	else if (m->map[y][x] == 'C')
+		m->c_cnt++;
+    valid_path(m, x + 1, y);
+    valid_path(m, x - 1, y);
+    valid_path(m, x, y + 1);
+    valid_path(m, x, y - 1);
+}
+
 void	check_valid_path(t_map *m)
 {
 	visited_init(m);
+	valid_path(m, m->p_x, m->p_y);
+	if (m->e_flag == FALSE || m->c_cnt != m->c)
+		error_exit("There is no valid path in the map.\n");
 }
