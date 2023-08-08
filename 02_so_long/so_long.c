24fc112cf42ce2633a5c7d2b0a028352696bb5b5
이지윤 <02leejiyun@naver.com>
2023-08-08 17:26:17 +0900

    refactor: [so_long.c] so_long.c 리팩토링

--- 02_so_long/so_long.c
+++ 02_so_long/so_long.c
@@ -6,24 +6,18 @@
 /*   By: jiyunlee <jiyunlee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 20:29:54 by jiyunlee          #+#    #+#             */
-/*   Updated: 2023/08/03 02:13:44 by jiyunlee         ###   ########.fr       */
+/*   Updated: 2023/08/07 21:16:24 by jiyunlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	check_leak(void)
-{
-	system("leaks -s a.out");
-}
-
 int	main(int argc, char **argv)
 {
 	static t_map	m;
 
-	atexit(check_leak);
 	check_valid_file(argc, argv);
 	check_valid_map(argv[1], &m);
-	// check_valid_path(&m);
-	// printf("c: %d, e: %d, p: %d\n", m.c, m.e, m.p);
+	check_valid_path(&m);
+	play_game(&m);
 }
