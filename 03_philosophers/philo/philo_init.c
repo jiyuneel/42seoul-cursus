f3c8ff3f6b0f5390f48ece007c82573ebb1c83b1
Jiyun Lee <02leejiyun@naver.com>
2023-08-18 21:18:25 +0900

    add: [philo_init.c] philoc_init.c 파일 추가

---
+++ 03_philosophers/philo/philo_init.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_init.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jiyunlee <jiyunlee@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/18 16:24:12 by jiyunlee          #+#    #+#             */
+/*   Updated: 2023/08/18 19:50:54 by jiyunlee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	mutex_init(t_data *data);
+
+int	philo_init(t_data *data, int argc, char **argv)
+{
+	// 문자가 들어왔을 경우 예외처리
+	data->number_of_philos = ft_atoi(argv[1]);
+	data->time_to_die = ft_atoi(argv[2]);
+	data->time_to_eat = ft_atoi(argv[3]);
+	data->time_to_sleep = ft_atoi(argv[4]);
+	if (argc == 6)
+		data->number_of_times_eat = ft_atoi(argv[5]);
+	if (data->number_of_philos <= 0 || data->time_to_die < 0
+		||data->time_to_eat < 0 || data->time_to_sleep < 0
+		|| data->number_of_times_eat <= 0)
+		return (EXIT_FAILURE);
+	mutex_init(data);
+	return (EXIT_SUCCESS);
+}
+
+int	mutex_init(t_data *data)
+{
+	int	i;
+
+	if (!pthread_mutex_init(&(data->print), NULL))
+		return (EXIT_FAILURE);
+	data->forks = malloc(sizeof(pthread_mutex_t) * data->number_of_philos);
+	if (!data->forks)
+		return (EXIT_FAILURE);
+	while (i < data->number_of_philos)
+	{
+		if (!pthread_mutex_init(&(data->forks[i]), NULL))
+			return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
