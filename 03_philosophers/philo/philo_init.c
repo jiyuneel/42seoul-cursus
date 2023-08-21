57f304bbb0d43d09ef58c3723ad793dcd74cc1f9
이지윤 <02leejiyun@naver.com>
2023-08-21 23:32:31 +0900

    feat: [philo_init.c] argument에 문자가 들어오는 경우 처리

--- 03_philosophers/philo/philo_init.c
+++ 03_philosophers/philo/philo_init.c
@@ -6,7 +6,7 @@
 /*   By: jiyunlee <jiyunlee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 16:24:12 by jiyunlee          #+#    #+#             */
-/*   Updated: 2023/08/18 19:50:54 by jiyunlee         ###   ########.fr       */
+/*   Updated: 2023/08/21 21:56:01 by jiyunlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,19 @@ int	mutex_init(t_data *data);
 
 int	philo_init(t_data *data, int argc, char **argv)
 {
-	// 문자가 들어왔을 경우 예외처리
 	data->number_of_philos = ft_atoi(argv[1]);
 	data->time_to_die = ft_atoi(argv[2]);
 	data->time_to_eat = ft_atoi(argv[3]);
 	data->time_to_sleep = ft_atoi(argv[4]);
-	if (argc == 6)
-		data->number_of_times_eat = ft_atoi(argv[5]);
 	if (data->number_of_philos <= 0 || data->time_to_die < 0
-		||data->time_to_eat < 0 || data->time_to_sleep < 0
-		|| data->number_of_times_eat <= 0)
+		||data->time_to_eat < 0 || data->time_to_sleep < 0)
 		return (EXIT_FAILURE);
+	if (argc == 6)
+	{
+		data->number_of_times_eat = ft_atoi(argv[5]);
+		if (data->number_of_times_eat <= 0)
+			return (EXIT_FAILURE);
+	}
 	mutex_init(data);
 	return (EXIT_SUCCESS);
 }
--- 03_philosophers/philo/philo_utils.c
+++ 03_philosophers/philo/philo_utils.c
@@ -6,7 +6,7 @@
 /*   By: jiyunlee <jiyunlee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 18:50:50 by jiyunlee          #+#    #+#             */
-/*   Updated: 2023/08/18 19:22:58 by jiyunlee         ###   ########.fr       */
+/*   Updated: 2023/08/21 17:21:48 by jiyunlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,23 +22,38 @@ int	ft_isdigit(int c)
 int	ft_atoi(const char *str)
 {
 	long long	num;
-	int			sign;
 
 	num = 0;
-	sign = 1;
-	while ((9 <= *str && *str <= 13) || *str == ' ')
-		str++;
-	if (*str == '+')
-		str++;
-	else if (*str == '-')
+	while (*str)
 	{
-		sign = -1;
-		str++;
-	}
-	while (*str && ft_isdigit(*str))
-	{
+		if (!ft_isdigit(*str))
+			return (-1);
 		num = num * 10 + (*str - '0');
 		str++;
 	}
-	return (sign * num);
+	return (num);
 }
+
+// int	ft_atoi(const char *str)
+// {
+// 	long long	num;
+// 	int			sign;
+
+// 	num = 0;
+// 	sign = 1;
+// 	while ((9 <= *str && *str <= 13) || *str == ' ')
+// 		str++;
+// 	if (*str == '+')
+// 		str++;
+// 	else if (*str == '-')
+// 	{
+// 		sign = -1;
+// 		str++;
+// 	}
+// 	while (*str && ft_isdigit(*str))
+// 	{
+// 		num = num * 10 + (*str - '0');
+// 		str++;
+// 	}
+// 	return (sign * num);
+// }
