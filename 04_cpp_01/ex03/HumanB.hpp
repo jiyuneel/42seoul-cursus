89e1107114bba8d78b83439d582f98a85e9f6956
이지윤 <02leejiyun@naver.com>
2023-11-27 17:33:00 +0900

    feat: [cpp01] ex03 구현

---
+++ 04_cpp_01/ex03/HumanB.hpp
@@ -0,0 +1,20 @@
+#ifndef HUMAN_B_HPP
+#define HUMAN_B_HPP
+
+#include <iostream>
+#include "Weapon.hpp"
+
+class HumanB {
+private:
+    std::string _name;
+    Weapon* _weapon;
+
+public:
+    HumanB(std::string name);
+    ~HumanB();
+
+    void setWeapon(Weapon& weapon);
+    void attack();
+};
+
+#endif
