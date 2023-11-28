333f13a1074c8a5abda82c26ea21f07a999613fd
이지윤 <02leejiyun@naver.com>
2023-11-28 10:02:47 +0900

    feat: [cpp01] ex04 구현

---
+++ 04_cpp_01/ex04/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+
+void replaceStr(std::string& str, std::string s1, std::string s2) {
+    size_t pos = 0;
+    while (true) {
+        pos = str.find(s1, pos);
+        if (pos == std::string::npos)
+            break;
+        str = str.substr(0, pos) + s2 + str.substr(pos + s1.length());
+        pos += s2.length();
+    }
+}
+
+int main(int argc, char **argv) {
+    if (argc != 4) {
+        std::cout << "ERROR: Invalid parameter" << std::endl;
+        return 1;
+    }
+
+    std::string filename(argv[1]);
+    std::ifstream infile(filename);
+    if (!infile.is_open()) {
+        std::cout << "ERROR: Cannot open file" << std::endl;
+        return 1;
+    }
+    std::ofstream outfile(filename + ".replace");
+    if (!outfile.is_open()) {
+        std::cout << "ERROR: Cannot open file" << std::endl;
+        return 1;
+    }
+
+    std::string s1(argv[2]);
+    std::string s2(argv[3]);
+    if (s1.length() == 0) {
+        std::cout << "ERROR: Invalid string" << std::endl;
+        return 1;
+    }
+
+    std::string str;
+    while (getline(infile, str)) {
+        replaceStr(str, s1, s2);
+        outfile << str;
+        if (!infile.eof())
+            outfile << std::endl;
+    }
+    infile.close();
+    outfile.close();
+}
