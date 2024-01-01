73883320b50fc07b6b7254dcc03a569b4b321a38
이지윤 <02leejiyun@naver.com>
2024-01-01 20:04:51 +0900

    feat: [cpp02] ex01 구현

---
+++ 04_cpp_02/ex01/Fixed.cpp
@@ -0,0 +1,53 @@
+#include "Fixed.hpp"
+
+/* Default constructor */
+Fixed::Fixed() : _num(0) {
+    std::cout << "Default constructor called" << std::endl;
+}
+Fixed::Fixed(const int value) {
+    std::cout << "Int constructor called" << std::endl;
+    this->_num = value << this->_bit;
+}
+Fixed::Fixed(const float value) {
+    std::cout << "Float constructor called" << std::endl;
+    this->_num = roundf(value * (1 << this->_bit));
+}
+
+/* Copy constructor */
+Fixed::Fixed(const Fixed& fixed) {
+    std::cout << "Copy constructor called" << std::endl;
+    *this = fixed;
+}
+
+/* Copy assignment operator */
+Fixed& Fixed::operator=(const Fixed& fixed) {
+    std::cout << "Copy assignment operator called" << std::endl;
+    if (this != &fixed) {
+        this->_num = fixed.getRawBits();
+    }
+    return (*this);
+}
+std::ostream& operator<<(std::ostream& out, const Fixed& fixed) {
+    return out << fixed.toFloat();
+}
+
+/* Destructor */
+Fixed::~Fixed() {
+    std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits( void ) const{
+    return this->_num;
+}
+
+void Fixed::setRawBits( int const raw ) {
+    this->_num = raw;
+}
+
+float Fixed::toFloat( void ) const {
+    return (float)this->_num / (1 << this->_bit);
+}
+
+int Fixed::toInt( void ) const {
+    return this->_num >> this->_bit;
+}
---
+++ 04_cpp_02/ex01/Fixed.hpp
@@ -0,0 +1,27 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed {
+private:
+    int _num;
+    static const int _bit = 8;
+
+public:
+    Fixed();
+    Fixed(const int value);
+    Fixed(const float value);
+    Fixed(const Fixed& fixed);
+    Fixed& operator=(const Fixed& fixed);
+    ~Fixed();
+
+    int getRawBits( void ) const;
+    void setRawBits( int const raw );
+    float toFloat( void ) const;
+    int toInt( void ) const;
+};
+
+std::ostream& operator<<(std::ostream& out, const Fixed& fixed);
+
+#endif
